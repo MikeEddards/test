ba404f14d9613464fa6a7e49f0b0817fe82b778f
CODE\mike.eddards <mike.eddards@codecorp.com>
2022-07-14 13:48:41 -0600

    ready

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const pgp = require('pg-promise')()
+const { DATABASE_URL} = process.env
+const db = pgp(DATABASE_URL)
+
+db.connect().then(res=>{
+    console.log(res.client.serverVersion)
+    res.done()
+}).catch(err=>{
+    console.log(err)
+})
+
+
+const app = express();
+app.use(express.json())
+app.get('/', (req,res)=>{
+	res.send("<h1>Hello There</h1>")
+});
+app.post('/api/scanned',async (req,res)=>{
+	let {data}=req.body
+	await db.any('INSERT INTO public.data_test (data) VALUES($1);',[data])
+	console.log(`Storing ${data}`)
+})
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
