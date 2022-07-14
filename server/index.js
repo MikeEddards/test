fcbe9b77a060bd8860ce0955cef146707421458d
CODE\mike.eddards <mike.eddards@codecorp.com>
2022-07-14 14:01:54 -0600

    adsgf

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 const express = require('express');
 const pgp = require('pg-promise')()
 const { DATABASE_URL} = process.env
